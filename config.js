d804e51c18bb8c49b1d9d7c3d8fc1705b68a3a17
BENTIL <bentiljoe9@gmail.com>
2026-02-20 16:11:46 +0000

    version2

--- config.js
+++ config.js
@@ -7,3 +7,5 @@ function version2(str1, str2){
     return str1 + str2
 }
 version2("people","lord")
+
+console.log("version3")
--- src/index.js
+++ src/index.js
@@ -1,11 +1,17 @@
 var version1={
     make: "Toyota",
     model: "Camry",
-    year: 2020, 
+    year: 2020 
 }
 
 var version2={
     make: "Honda",
     model: "Civic",
     year: 2021, 
+}
+
+var version3={
+    people: "John Doe",
+    lord: "Jane Smith",
+    Age: 30
 }
