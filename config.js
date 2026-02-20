07e7f7936b05c3aa53dbf790bc007ff2ce9815ac
BENTIL <bentiljoe9@gmail.com>
2026-02-20 16:06:00 +0000

    version1

---
+++ config.js
@@ -0,0 +1,9 @@
+function version1(a,b){
+    return a+b
+}
+version1("Hello, ","World!")
+
+function version2(str1, str2){
+    return str1 + str2
+}
+version2("people","lord")
---
+++ src/index.js
@@ -0,0 +1,11 @@
+var version1={
+    make: "Toyota",
+    model: "Camry",
+    year: 2020, 
+}
+
+var version2={
+    make: "Honda",
+    model: "Civic",
+    year: 2021, 
+}
